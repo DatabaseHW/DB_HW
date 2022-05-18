49423142e40116b035d4fa4beaef1db965f541c6
roy <roylin506@gmail.com>
2022-05-18 13:34:18 +0800

    set flask to run on localhost:5000

--- main.py
+++ main.py
@@ -14,5 +14,5 @@ from configuration import website, user_database
 
 if __name__ == '__main__':
     # user_database.drop_all()    # 希望程式重啟資料庫不會被刪除，就把這行註解
-    # user_database.create_all()
+    user_database.create_all()
     website.run(host="0.0.0.0", port=5000)

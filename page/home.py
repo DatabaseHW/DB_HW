223e42aa8df18e19ced780dec19a648a0d99844b
vincent97198 <vincent97198@gmail.com>
2022-05-19 02:03:15 +0800

    show and add product including picture done

--- page/home.py
+++ page/home.py
@@ -28,9 +28,9 @@ def home():
             flash("商店不存在", category="Product add errors")
             return render_template(
                                     'nav.html', 
-                                    shop_product_form = Shop.query.join(Shop, Shop.sid == Product.sid and Shop.pid == current_user.get_id()).add_columns(Product.name, Product.pid, Product.price, Product.quantity, Product.picture),
-                                    shop_form = Shop_Form, 
-                                    name_repeated=True, 
+                                    shop_product = Shop.query.outerjoin(Shop, Shop.sid == Product.sid and Shop.pid == current_user.get_id()).add_columns(Product.name, Product.pid, Product.price, Product.quantity, Product.picture),
+                                    shop_form = Shop_Form,
+                                    product_form = Product_Form,
                                     user = User.query.filter_by(id=current_user.get_id()).first(), 
                                     has_shop=Shop.query.filter_by(uid=current_user.get_id())
                                 )
@@ -39,32 +39,34 @@ def home():
             flash("商品已經存在", category="Product add errors")
             return render_template(
                                     'nav.html', 
-                                    shop_product_form = Shop.query.join(Shop, Shop.sid == Product.sid and Shop.pid == current_user.get_id()).add_columns(Product.name, Product.pid, Product.price, Product.quantity, Product.picture),
+                                    shop_product = Shop.query.outerjoin(Product, Shop.sid == Product.sid and Shop.pid == current_user.get_id()).add_columns(Product.name, Product.pid, Product.price, Product.quantity, Product.picture),
                                     shop_form = Shop_Form, 
-                                    name_repeated=True, 
+                                    product_form = Product_Form,
                                     user = User.query.filter_by(id=current_user.get_id()).first(), 
                                     has_shop=Shop.query.filter_by(uid=current_user.get_id())
                                 )
         else:
-            images = base64.b64encode(request.files['picture'].read()).decode('ascii')
+            images = base64.b64encode(request.files['picture'].read())
+            if images == b'':
+                images = None
             new_product = Product(selling_shop.sid, Product_Form.name.data, Product_Form.quantity.data, Product_Form.price.data, images)
             user_database.session.add(new_product)
             user_database.session.commit()
             flash("新增成功",category="product add success")
             return render_template(
                                     'nav.html', 
-                                    shop_product_form = Shop.query.join(Shop, Shop.sid == Product.sid and Shop.pid == current_user.get_id()).add_columns(Product.name, Product.pid, Product.price, Product.quantity, Product.picture),
+                                    shop_product = Shop.query.outerjoin(Product, Shop.sid == Product.sid and Shop.pid == current_user.get_id()).add_columns(Product.name, Product.pid, Product.price, Product.quantity, Product.picture),
                                     shop_form = Shop_Form, 
-                                    name_repeated=False, 
+                                    product_form = Product_Form,
                                     user = User.query.filter_by(id=current_user.get_id()).first(), 
                                     has_shop=Shop.query.filter_by(uid=current_user.get_id())
                                 )
 
     return render_template(
                             "nav.html", 
-                            shop_product_form = Shop.query.join(Shop, Shop.sid == Product.sid and Shop.pid == current_user.get_id()).add_columns(Product.name, Product.pid, Product.price, Product.quantity, Product.picture),
+                            shop_product = Shop.query.outerjoin(Product, Shop.sid == Product.sid and Shop.pid == current_user.get_id()).add_columns(Product.name, Product.pid, Product.price, Product.quantity, Product.picture),
                             shop_form = Shop_Form, 
-                            name_repeated=False, 
+                            product_form = Product_Form,
                             user = User.query.filter_by(id=current_user.get_id()).first(), 
                             has_shop=Shop.query.filter_by(uid=current_user.get_id())
                         )
--- page/shop_register.py
+++ page/shop_register.py
@@ -8,15 +8,17 @@ from configuration import website, user_database
 from forms import LoginForm, ShopForm, ProductForm
 from model.shop import Shop
 from model.user import User
+from model.product import Product
 
 def shop_register(Shop_Form, Product_Form):
     shop = Shop.query.filter_by(name=Shop_Form.name.data).first()
     if shop is not None:
+        flash("商店名稱不可重複",category="name_repeated")
         return render_template(
                                     'nav.html', 
-                                    shop_product_form = Shop.query.join(Shop, Shop.sid == Product.sid and Shop.pid == current_user.get_id()).add_columns(Product.name, Product.pid, Product.price, Product.quantity, Product.picture),
+                                    shop_product = Shop.query.outerjoin(Product, Shop.sid == Product.sid and Shop.pid == current_user.get_id()).add_columns(Product.name, Product.pid, Product.price, Product.quantity, Product.picture),
                                     shop_form = Shop_Form, 
-                                    name_repeated=True, 
+                                    product_form = Product_Form,
                                     user = User.query.filter_by(id=current_user.get_id()).first(), 
                                     has_shop=Shop.query.filter_by(uid=current_user.get_id())
                                 )
@@ -27,9 +29,9 @@ def shop_register(Shop_Form, Product_Form):
         flash("註冊成功",category="register success")
         return render_template(
                                     'nav.html', 
-                                    shop_product_form = Shop.query.join(Shop, Shop.sid == Product.sid and Shop.pid == current_user.get_id()).add_columns(Product.name, Product.pid, Product.price, Product.quantity, Product.picture),
+                                    shop_product = Shop.query.outerjoin(Product, Shop.sid == Product.sid and Shop.pid == current_user.get_id()).add_columns(Product.name, Product.pid, Product.price, Product.quantity, Product.picture),
                                     shop_form = Shop_Form, 
-                                    name_repeated=False, 
+                                    product_form = Product_Form,
                                     user = User.query.filter_by(id=current_user.get_id()).first(), 
                                     has_shop=Shop.query.filter_by(uid=current_user.get_id())
                                 )
